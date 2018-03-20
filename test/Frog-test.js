f3a9cdabdc11032baca05e0f8607653ab4a1b5fc
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-03-19 23:39:49 -0600

    Add log tests for properties and movement of log and movement of frog

--- test/Frog-test.js
+++ test/Frog-test.js
@@ -2,26 +2,26 @@ const assert = require('chai').assert;
 const Frog = require('../lib/Frog.js');
 
 describe('Frog', function() {
+
+  beforeEach(() => {
+    var frog = new Frog(10, 40, 20, 20, 'blue');
+  });
+
   it('should be a function', function() {
     assert.isFunction(Frog);
   });
 
-  it('should instantiate a new Frog', function() {
-    var frog = new Frog;
-    assert.isObject(frog);
-  });
-
   it('should have dimensions', function() {
-    var frog = new Frog(10, 40, 20, 20);
     assert.equal(frog.x, 10);
     assert.equal(frog.y, 40);
     assert.equal(frog.width, 20);
     assert.equal(frog.height, 20);
+    assert.equal(frog.color), 'blue'
   });
 
-  it('should be able to hop up', function() {
-    var frog = new Frog(10, 40, 20, 20);
-    frog.hopUp();
-    assert.equal(frog.y, 30);
-  });
+  // it('should be able to hop up', function() {
+    
+  //   frog.hopUp();
+  //   assert.equal(frog.y, 30);
+  // });
 });
--- test/Log-test.js
+++ test/Log-test.js
@@ -1,68 +1,118 @@
-const chai = require('chai');
-const assert = chai.assert;
-// const assert = require('chai').assert;
-const Log = require('../lib/Log.js');
-const Frog = require('../lib/Frog.js');
+var chai = require('chai');
+var assert = chai.assert;
+var Log = require('../lib/Log');
+var Frog = require('../lib/Frog')
 
-describe ('Log', function() {
-  it('should be a function', function() {
-    assert.isFunction(Log);
+describe('Log', () => {
+  var log;
+  var frog;
+
+  beforeEach(() => {
+    log = new Log(400, 30, 80, 25, 'blue');
   });
 
-  it('should instantiate a new log', function() {
-    var log = new Log;
-    assert.isObject(log);
-  });
+  it('has properties', () => {
+    assert.equal(log.x, 400);
+    assert.equal(log.y, 30);
+    assert.equal(log.width, 80);
+    assert.equal(log.height, 25);
+  })
 
-  it('should have dimensions', function() {
-    var log = new Log(10, 10, 20, 20);
-    assert.equal(log.x, 10);
-    assert.equal(log.y, 10);
-    assert.equal(log.width, 20);
-    assert.equal(log.height, 20);
-  });
-
-  it('should check if Frog is on it', function() {
-    var log = new Log(10, 10, 20, 20);
-    var frog = new Frog(10, 40, 20, 20);
-    log.collides(frog);
+  it('has default property, collision, of false ', () => {
     assert.equal(log.collision, false);
-    frog.hopUp();//frog touching
-    frog.hopUp();//frog halfway
-    frog.hopUp();//frog fully on
-    log.collides(frog);
+  })
+
+  it('should confirm collision if x and y coordinates are the same', () => {
+    assert.equal(log.collision, false);
+    frog = new Frog(400, 30, 80, 25);
+    log.checkForFrog(frog);
     assert.equal(log.collision, true);
-    assert.equal(log.y, frog.y);
-    frog.hopUp();
-    frog.hopUp();
-    frog.hopUp();
-    log.collides(frog);
-    assert.equal(log.collision, false);
   });
 
-  it('should move left', function() {
-    var log = new Log(20, 20, 20, 20);
+  it('should move left', () => {
+    // var log = new Log(20, 30, 80, 25);
     log.moveLeft();
     log.moveLeft();
-    assert.equal(log.x, 0);
+    assert.equal(log.x, 398);
   });
 
-  it('should let the frog ride', function() {
-    var log = new Log(10, 10, 60, 20);
-    var frog = new Frog(30, 40, 20, 20);
-    log.collides(frog);
+  it('should let the frog ride', () => {
     assert.equal(log.collision, false);
-    frog.hopUp();//frog touching
-    frog.hopUp();//frog halfway
-    frog.hopUp();//frog fully on
-    log.collides(frog);
+    frog = new Frog(400, 30, 40, 40);
+    log.checkForFrog(frog);
+
     assert.equal(log.collision, true);
-    assert.equal(log.y, frog.y);;
-    assert.equal(frog.x, 20);
-    log.moveLeft();//to be looped
-    log.collides(frog);//to be looped
-    assert.equal(log.x, 0);
-    assert.equal(frog.x, 10);
+
+    log.moveLeft();
+    
+    assert.equal(log.x, 399);
+    assert.equal(frog.x, 399);
   });
+
+
 });
+
+
+
+//_____________________________________________________________________________
+
+
+// describe ('Log', function() {
+//   it('should be a function', function() {
+//     assert.isFunction(Log);
+//   });
+
+
+
+//   it('should have dimensions', function() {
+//     var log = new Log(10, 10, 20, 20);
+//     assert.equal(log.x, 10);
+//     assert.equal(log.y, 10);
+//     assert.equal(log.width, 20);
+//     assert.equal(log.height, 20);
+//   });
+
+//   it('should check if Frog is on it', function() {
+//     var log = new Log(10, 10, 20, 20);
+//     var frog = new Frog(10, 40, 20, 20);
+//     log.collides(frog);
+//     assert.equal(log.collision, false);
+//     frog.hopUp();//frog touching
+//     frog.hopUp();//frog halfway
+//     frog.hopUp();//frog fully on
+//     log.collides(frog);
+//     assert.equal(log.collision, true);
+//     assert.equal(log.y, frog.y);
+//     frog.hopUp();
+//     frog.hopUp();
+//     frog.hopUp();
+//     log.collides(frog);
+//     assert.equal(log.collision, false);
+//   });
+
+//   it('should move left', function() {
+//     var log = new Log(20, 20, 20, 20);
+//     log.moveLeft();
+//     log.moveLeft();
+//     assert.equal(log.x, 0);
+//   });
+
+//   it('should let the frog ride', function() {
+//     var log = new Log(10, 10, 60, 20);
+//     var frog = new Frog(30, 40, 20, 20);
+//     log.collides(frog);
+//     assert.equal(log.collision, false);
+//     frog.hopUp();//frog touching
+//     frog.hopUp();//frog halfway
+//     frog.hopUp();//frog fully on
+//     log.collides(frog);
+//     assert.equal(log.collision, true);
+//     assert.equal(log.y, frog.y);;
+//     assert.equal(frog.x, 20);
+//     log.moveLeft();//to be looped
+//     log.collides(frog);//to be looped
+//     assert.equal(log.x, 0);
+//     assert.equal(frog.x, 10);
+//   });
+// });
 
